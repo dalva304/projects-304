a5634848a181f7199fe389817c79c21a29d32d54
daalva03 <diana.alvarez03@utrgv.edu>
2024-10-07 22:02:19 -0500

    adjusting the clicker functions !

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ window.addEventListener("load", function ()
     //get tag reference
     let clickcountertag = document.getElementById("clickcounter");
     let clickbuttontag = document.getElementById("clickbutton");
+    let congratsMessageTag = document.getElementById("congratsMessage"); // Add a tag to display the message
 
     //define a function for updating the click counter.
     let clickbuttonupdate= function()
@@ -12,6 +13,11 @@ window.addEventListener("load", function ()
         numberofclicks++;
 
         clickcountertag.innerText = numberofclicks;
+
+        // Display congrats message after 10 clicks
+        if (numberofclicks === 10) {
+            congratsMessageTag.innerText = "Congrats!";
+        }
     };
 
     //call function every time the button is clicked
